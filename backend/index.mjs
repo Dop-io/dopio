c310647ac6ac27c47eaec3713a4fa8eed6ab1970
Nim1com <57798165+Nim1com@users.noreply.github.com>
2022-01-16 11:27:29 +0200

    Update index.mjs

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,10 +1,11 @@
 import { NFTStorage, File } from 'nft.storage'
 const client = new NFTStorage({ token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDNhRjdhQkU5QkQzRkYwMjFCNjEzQjgwOTJBOTYwN2REMEIyMzQ4OUEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0MjE4NzA0ODgwNywibmFtZSI6ImRvcGlvMSJ9.xZavvnxVNIQH8ji3-L99kFbYbc2_8VEiZA0TlMopJ_Y' })
+const punkvar = '0001'
 
 async function main() {
   const metadata = await client.store({
     name: 'CryptoDop',
-    punkid: 'punkvar',
+    punkid: punkvar,
     description: 'Crypto Dops are user made editions of Crypto Punks.',
     image: new File(
       [
