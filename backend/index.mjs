971803acdf4dee889e57c2d3cfa8b21f89373ff1
Nim1com <niklas@wojtkowiak.com>
2022-01-16 11:26:03 +0200

    donee

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,12 +1,22 @@
 import { NFTStorage, File } from 'nft.storage'
-import { pack } from 'ipfs-car/pack';
+const client = new NFTStorage({ token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDNhRjdhQkU5QkQzRkYwMjFCNjEzQjgwOTJBOTYwN2REMEIyMzQ4OUEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0MjE4NzA0ODgwNywibmFtZSI6ImRvcGlvMSJ9.xZavvnxVNIQH8ji3-L99kFbYbc2_8VEiZA0TlMopJ_Y' })
 
-const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDNhRjdhQkU5QkQzRkYwMjFCNjEzQjgwOTJBOTYwN2REMEIyMzQ4OUEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0MjE4NzA0ODgwNywibmFtZSI6ImRvcGlvMSJ9.xZavvnxVNIQH8ji3-L99kFbYbc2_8VEiZA0TlMopJ_Y'
-const client = new NFTStorage({ token: apiKey })
+async function main() {
+  const metadata = await client.store({
+    name: 'CryptoDop',
+    punkid: 'punkvar',
+    description: 'Crypto Dops are user made editions of Crypto Punks.',
+    image: new File(
+      [
+        /* data */
+      ],
+      'index.jpg',
+      { type: 'image/jpg' }
+    ),
+  })
+  console.log(metadata.url)
+  // ipfs://bafyreib4pff766vhpbxbhjbqqnsh5emeznvujayjj4z2iu533cprgbz23m/metadata.json
+}
 
-const metadata = await client.store({
-  name: 'test1',
-  description: 'testdescription1',
-  
-})
-console.log(metadata.url)
+main()
+
